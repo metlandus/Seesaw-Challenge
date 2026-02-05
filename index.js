fa9733c28a1a9031675ee417ec61a69b08658cac
Mete <metlandus@hotmail.com.tr>
2026-02-05 22:02:49 +0300

    Now creating a ball according to the mouse hover

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+const seesawPlank = document.querySelector(".plank");
+
+const clickableArea = document.querySelector(".clickable-area");
+
+function createNewBall() {
+	clickableArea.insertAdjacentHTML(
+		"afterbegin",
+		"<div class=new-ball></div>",
+	);
+}
+clickableArea.addEventListener("mouseenter", createNewBall);
+clickableArea.addEventListener("mouseleave", () => {
+	const ball = document.querySelector(".new-ball");
+	clickableArea.removeChild(ball);
+});
+clickableArea.addEventListener("mousemove", (e) => {
+	const ball = document.querySelector(".new-ball");
+	ball.style.left = `${e.pageX}px`;
+	ball.style.top = `44%`;
+	console.log(e);
+});
