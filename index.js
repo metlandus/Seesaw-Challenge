325c73bd080062617a448b5bbba80607cd7e8290
Mete <metlandus@hotmail.com.tr>
2026-02-06 16:38:40 +0300

    Objects are created with weights, smoothly fall down and get registered to an array now

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const seesawPlank = document.querySelector(".plank");
-
 const clickableArea = document.querySelector(".clickable-area");
+const plank = document.createElement("div");
+plank.className = "plank";
+const plankHeight = 15;
+const plankWidth = 500;
+plank.style.height = `${plankHeight}px`;
+plank.style.width = `${plankWidth}px`;
+clickableArea.appendChild(plank);
+
+// const mainframe = document.querySelector(".main-frame");
+// const pivot = document.createElement("div");
+// pivot.className = "pivot";
+// mainframe.appendChild(pivot);
+
+let weights = [];
 
 function createNewBall() {
 	clickableArea.insertAdjacentHTML(
@@ -8,14 +20,43 @@ function createNewBall() {
 		"<div class=new-ball></div>",
 	);
 }
+function generateRandomObj() {
+	const nextMass = Math.floor(Math.random() * 10 + 1);
+	console.log("Next Mass is :", nextMass);
+	return nextMass;
+}
+
+// Here is for creating a "ghost"
 clickableArea.addEventListener("mouseenter", createNewBall);
 clickableArea.addEventListener("mouseleave", () => {
 	const ball = document.querySelector(".new-ball");
 	clickableArea.removeChild(ball);
 });
 clickableArea.addEventListener("mousemove", (e) => {
+
 	const ball = document.querySelector(".new-ball");
-	ball.style.left = `${e.pageX}px`;
-	ball.style.top = `44%`;
-	console.log(e);
+	ball.style.left = `${e.clientX - 10}px`;
+	ball.style.top = `${e.clientY - 20}px `;
+});
+
+clickableArea.addEventListener("click", (e) => {
+	const weight = generateRandomObj();
+	const newObj = document.createElement("div");
+	newObj.classList.add("new-obj");
+	newObj.style.left = `${e.clientX - 10}px`;
+	newObj.style.top = `${e.clientY - 20}px`;
+	newObj.style.width = `${weight * 2 + 20}px`;
+	newObj.style.height = `${weight * 2 + 20}px`;
+	newObj.style.transitionDuration = "700ms";
+	setTimeout(() => (newObj.style.top = `${70}%`), 0);
+	clickableArea.appendChild(newObj);
+	newObj.setAttribute("id", weights.length);
+	const side = e.clientX > plankWidth / 2 ? "right" : "left";
+	weights.push({
+		id: weights.length,
+		weight,
+		position: e.pageX,
+		side: side,
+	});
+	console.log(weights);
 });
