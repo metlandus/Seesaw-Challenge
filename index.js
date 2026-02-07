6232bdbea75ef1cb4ae949f75f604b3348b121c8
Mete <metlandus@hotmail.com.tr>
2026-02-07 19:11:17 +0300

    Successfully calculating torque and rotation

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ plank.style.height = `${plankHeight}px`;
 plank.style.width = `${plankWidth}px`;
 clickableArea.appendChild(plank);
 
+const rotation = document.querySelector(".rotation");
+
+// const body = document.querySelector("body");
+// body.addEventListener("mousemove", (e) => console.log(e.x));
+
 // const mainframe = document.querySelector(".main-frame");
 // const pivot = document.createElement("div");
 // pivot.className = "pivot";
@@ -20,30 +25,44 @@ function createNewBall() {
 		"<div class=new-ball></div>",
 	);
 }
+
+// const firstObj = generateRandomObj();
+
 function generateRandomObj() {
 	const nextMass = Math.floor(Math.random() * 10 + 1);
-	console.log("Next Mass is :", nextMass);
+	// console.log("Next Mass is :", nextMass);
 	return nextMass;
 }
-
 function calcTorque(weights) {
 	if (weights.length) {
 		let leftSide = weights.filter((e) => e.side === "left");
 		let rightSide = weights.filter((e) => e.side === "right");
 		let totalTorque = 0;
-		const plankMiddle = plankWidth / 2;
 		leftSide.forEach((e) => {
-			const left = e.weight * (plankMiddle - e.position);
-			return (totalTorque += left);
+			const left = e.weight * (plankWidth / 2 - e.position);
+			return (totalTorque -= left);
 		});
 
 		rightSide.forEach((e) => {
-			const right = e.weight * (e.position - plankMiddle);
-			return (totalTorque -= right);
+			const right = e.weight * (e.position - plankWidth / 2);
+			return (totalTorque += right);
 		});
-		console.log("🚀 ~ calcTorque ~ totalTorque:", totalTorque);
+		return totalTorque;
 	} else {
 		alert("There are no weight");
+	}
+}
+
+function calcRotation(totalTorque) {
+	console.log("Total Torque:", totalTorque);
+	let scale = Math.round(totalTorque / 400);
+
+	if (scale >= 15) {
+		return 15;
+	} else if (scale <= -15) {
+		return -15;
+	} else {
+		return scale;
 	}
 }
 
@@ -55,28 +74,43 @@ clickableArea.addEventListener("mouseleave", () => {
 });
 clickableArea.addEventListener("mousemove", (e) => {
 	const ball = document.querySelector(".new-ball");
-	ball.style.left = `${e.clientX - 10}px`;
-	ball.style.top = `${e.clientY - 20}px `;
+	ball.style.left = `${e.offsetX - 5}px`;
+	ball.style.top = `${e.offsetY - 10}px `;
 });
-
+// clickableArea.addEventListener("mousemove", (e) => console.log(e.x));
 clickableArea.addEventListener("click", (e) => {
 	const weight = generateRandomObj();
 	const newObj = document.createElement("div");
+	newObj.innerText = `${weight} kg`;
 	newObj.classList.add("new-obj");
-	newObj.style.left = `${e.clientX - 10}px`;
-	newObj.style.top = `${e.clientY - 20}px`;
+	newObj.style.left = `${e.offsetX - 5}px`;
+	newObj.style.top = `${e.offsetY - clickableArea.offsetHeight}px`;
 	newObj.style.width = `${weight * 2 + 30}px`;
 	newObj.style.height = `${weight * 2 + 30}px`;
 	newObj.style.transitionDuration = "700ms";
-	setTimeout(() => (newObj.style.top = `${70}%`), 0);
-	clickableArea.appendChild(newObj);
+	setTimeout(() => (newObj.style.top = `${0 - plankHeight}px`), 0);
+	plank.appendChild(newObj);
 	newObj.setAttribute("id", weights.length);
-	const side = e.clientX > plankWidth / 2 ? "right" : "left";
+	const side = e.offsetX > plankWidth / 2 ? "right" : "left";
 	weights.push({
 		id: weights.length,
 		weight,
-		position: e.clientX,
+		position: e.offsetX,
 		side: side,
 	});
-	calcTorque(weights);
+	const totalTorque = calcTorque(weights);
+	let rotation = calcRotation(totalTorque);
+	rotation += rotation;
+	// console.log("Here are the stats", {
+	// 	rotation,
+	// 	weight,
+	// 	rotationSide: rotation > 0 ? "right" : "left",
+	// 	position: e.offsetX,
+	// });
+	plank.style.transform = `rotate(${rotation}deg)`;
+	rotation.innerHTML = ` ${rotation} degree`;
+
+	// const allBalls = document.querySelectorAll(".new-obj");
+	// console.log("🚀 ~ allBalls:", allBalls);
+	// allBalls.forEach((e) => e.style.top === `${plank.getBoundingClientRect().y}%`);
 });
